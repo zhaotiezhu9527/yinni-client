0aadcedb86ac4a6576adb5acf2f57b5409161606
water <watershui122@gmail.com>
2023-06-24 19:17:09 +0700

    关于我们

--- yinni-ui/pages/about.vue
+++ yinni-ui/pages/about.vue
@@ -22,82 +22,114 @@
         />
         <div class="text-content">
           
-        Shopee adalah platform e-commerce terkemuka di Asia Tenggara dan Taiwan.
-        Diluncurkan pada tahun 2015, ini adalah platform yang disesuaikan untuk wilayah tersebut untuk memberikan pengalaman belanja online yang mudah, aman, dan cepat kepada pelanggan melalui dukungan pembayaran dan pemenuhan yang kuat.
+        <p>Shopee adalah platform e-commerce terkemuka di Asia Tenggara dan Taiwan.
+        Diluncurkan pada tahun 2015, ini adalah platform yang disesuaikan untuk wilayah
+         tersebut untuk memberikan pengalaman belanja online yang mudah, aman, 
+         dan cepat kepada pelanggan melalui dukungan pembayaran dan pemenuhan yang kuat.</p>
 
-        Kami percaya bahwa belanja online harus nyaman, mudah, dan menyenangkan.Inilah visi yang ingin diwujudkan Shopee di platform setiap hari.
+        <p>Kami percaya bahwa belanja online harus nyaman, mudah, dan menyenangkan.
+          Inilah visi yang ingin diwujudkan Shopee di platform setiap hari.</p>
 
-        Jelajahi kulit udang
-        Tujuan kami
-        Kami percaya pada kekuatan transformatif teknologi dan berharap dapat menjadikan dunia tempat yang lebih baik dengan menyediakan platform untuk menghubungkan pembeli dan penjual di komunitas yang sama.
+        <p>Jelajahi kulit udang</p>
+        <p>Tujuan kami</p>
+        <p>Kami percaya pada kekuatan transformatif teknologi dan berharap dapat menjadikan 
+          dunia tempat yang lebih baik dengan menyediakan platform untuk menghubungkan pembeli 
+          dan penjual di komunitas yang sama.</p>
 
-        Posisi kami
-        Shopee memberikan pengalaman belanja online satu atap kepada pengguna Internet di wilayah tersebut, termasuk berbagai pilihan produk, komunitas sosial untuk eksplorasi, dan layanan pemenuhan tanpa batas.
+        <p>Posisi kami</p>
+        <p>Shopee memberikan pengalaman belanja online satu atap kepada pengguna Internet 
+          di wilayah tersebut, termasuk berbagai pilihan produk, komunitas sosial untuk eksplorasi,
+           dan layanan pemenuhan tanpa batas.</p>
 
-        Kepribadian kita
-        Untuk mendefinisikan siapa kita-bagaimana kita berbicara, berperilaku, atau bereaksi terhadap situasi tertentu-pada dasarnya, kita sederhana, bahagia, dan bersatu.Atribut utama ini terlihat di setiap langkah perjalanan Shopee.
+        <p>Kepribadian kita</p>
+        <p>Untuk mendefinisikan siapa kita-bagaimana kita berbicara, berperilaku, 
+          atau bereaksi terhadap situasi tertentu-pada dasarnya, kita sederhana, 
+          bahagia, dan bersatu.Atribut utama ini terlihat di setiap langkah perjalanan Shopee.</p>
 
-        sederhana
-        Kami percaya pada kesederhanaan dan integritas, memastikan kehidupan yang jujur, membumi, dan setia pada diri sendiri.
+        <p>sederhana</p>
+        <p>Kami percaya pada kesederhanaan dan integritas, memastikan kehidupan 
+          yang jujur, membumi, dan setia pada diri sendiri.</p>
 
-        happy
-        Kami ramah, lucu, dan energik, menyebarkan kebahagiaan kepada semua orang yang kami temui.
+        <p>happy</p>
+        <p>Kami ramah, lucu, dan energik, menyebarkan kebahagiaan kepada semua orang yang kami temui.</p>
 
-        bersama-sama
-        Kami suka berbelanja online dengan teman dan keluarga dan menghabiskan waktu bersama-melakukan apa yang kami sukai secara keseluruhan.
+        <p>bersama-sama</p>
+        <p>Kami suka berbelanja online dengan teman dan keluarga dan menghabiskan waktu 
+          bersama-melakukan apa yang kami sukai secara keseluruhan.</p>
 
-        Perjalanan kita
-        Meluncurkan Shopee di Singapura, Malaysia, Indonesia, Thailand, Taiwan, Vietnam, dan Filipina
-        Pada tahun 2015, Shopee diluncurkan di 7 pasar di wilayah tersebut.
+        <p>Perjalanan kita</p>
+        <p>Meluncurkan Shopee di Singapura, Malaysia, Indonesia, Thailand, Taiwan, Vietnam, dan Filipina
+        Pada tahun 2015, Shopee diluncurkan di 7 pasar di wilayah tersebut.</p>
 
-        Menyelenggarakan kursus Universitas Shopee pertama
-        Saat ini, penjual dapat dengan mudah memperoleh keterampilan dan pengetahuan baru untuk memahami cara mengembangkan bisnis online mereka dengan lebih baik melalui Shopee.
+        <p>Menyelenggarakan kursus Universitas Shopee pertama
+        Saat ini, penjual dapat dengan mudah memperoleh keterampilan dan pengetahuan 
+        baru untuk memahami cara mengembangkan bisnis online mereka dengan lebih baik melalui Shopee.</p>
 
-        Hari Belanja Super 9,9 pertama
-        Shopee meluncurkan 9.9 Super Shopping Day yang ikonik pada tahun 2016 untuk menarik pembeli mobile-first di Asia Tenggara dan Taiwan.
+        <p>Hari Belanja Super 9,9 pertama</p>
+        <p>Shopee meluncurkan 9.9 Super Shopping Day yang ikonik pada 
+          tahun 2016 untuk menarik pembeli mobile-first di Asia Tenggara dan Taiwan.</p>
 
-        Peluncuran Shopee Mall di seluruh wilayah
-        Shopee Mall kini menjadi mal online terkemuka di kawasan ini, menyediakan one-stop shopping untuk berbagai merek internasional dan lokal.
+        <p>Peluncuran Shopee Mall di seluruh wilayah
+        Shopee Mall kini menjadi mal online terkemuka di kawasan ini, 
+        menyediakan one-stop shopping untuk berbagai merek internasional dan lokal.</p>
 
-        Meluncurkan game dalam aplikasi Shopee pertama "Lucky Prize" untuk menghadirkan pengalaman berbelanja yang lebih menarik dan atraktif bagi pengguna
-        Lebih banyak game telah diluncurkan, seperti Shopee Farm dan Shopee Candy, membuat belanja menjadi lebih sosial dan berharga.
+        <p>Meluncurkan game dalam aplikasi Shopee pertama 
+          "Lucky Prize" untuk menghadirkan pengalaman berbelanja yang lebih menarik dan
+          atraktif bagi pengguna
+        Lebih banyak game telah diluncurkan, seperti Shopee Farm dan Shopee Candy, 
+        membuat belanja menjadi lebih sosial dan berharga.</p>
 
-        Meluncurkan ShopeePay, dompet seluler Shopee, dari Indonesia pada tahun 2018
-        ShopeePay saat ini menyediakan layanan kepada pengguna di Singapura, Malaysia, Thailand, Vietnam, dan Filipina.
+        <p>Meluncurkan ShopeePay, dompet seluler Shopee, 
+          dari Indonesia pada tahun 2018
+        ShopeePay saat ini menyediakan layanan kepada pengguna di Singapura, 
+        Malaysia, Thailand, Vietnam, dan Filipina.</p>
 
-        Meluncurkan Shopee Live, fungsi siaran langsung dalam aplikasi kami
-        Fitur ini menyediakan hiburan dan partisipasi langsung, memungkinkan pembeli dan penjual untuk menghubungi secara real time.
+        <p>Meluncurkan Shopee Live, fungsi siaran langsung dalam aplikasi kami
+        Fitur ini menyediakan hiburan dan partisipasi langsung, 
+        memungkinkan pembeli dan penjual untuk menghubungi secara real time.</p>
 
-        Mendirikan kantor pusat regional di Singapura
-        Kantor baru ini merupakan tonggak penting dalam proses pengembangan kami, memungkinkan kami untuk mengembangkan bakat ilmiah dan teknologi, mengembangkan ekosistem kewirausahaan, dan menciptakan lebih banyak peluang kerja.Bangunan ini meliputi area seluas 244.000 kaki persegi dan dapat menampung hingga 3.000 karyawan.
+        <p>Mendirikan kantor pusat regional di Singapura
+        Kantor baru ini merupakan tonggak penting dalam proses pengembangan kami, 
+        memungkinkan kami untuk mengembangkan bakat ilmiah dan teknologi,
+         mengembangkan ekosistem kewirausahaan, dan menciptakan lebih banyak peluang kerja.
+         Bangunan ini meliputi area seluas 244.000 kaki persegi dan dapat menampung hingga 3.000 karyawan.</p>
 
-        Meluncurkan Shopee di Brasil
-        Ini menandai pertama kalinya kami menjalankan bisnis di pasar selain Asia Tenggara dan Taiwan.
+        <p>Meluncurkan Shopee di Brasil
+        Ini menandai pertama kalinya kami menjalankan bisnis di pasar selain Asia Tenggara dan Taiwan.</p>
 
-        Meluncurkan Liga Kode Shopee
-        Sebagai aliansi kode online terbesar di kawasan ini, acara ini berharap dapat mengembangkan bakat teknis dan peminat serta memungkinkan mereka terhubung dengan orang-orang di seluruh dunia.
+        <p>Meluncurkan Liga Kode Shopee
+        Sebagai aliansi kode online terbesar di kawasan ini, 
+        acara ini berharap dapat mengembangkan bakat teknis dan 
+        peminat serta memungkinkan mereka terhubung dengan orang-orang di seluruh dunia.</p>
 
-        Meluncurkan Shopee Premium
-        Fitur ini sekarang terbuka untuk pengguna di Singapura, Malaysia, Thailand, Indonesia, dan Vietnam
+        <p>Meluncurkan Shopee Premium
+        Fitur ini sekarang terbuka untuk pengguna di Singapura, Malaysia, Thailand, Indonesia, dan Vietnam</p>
 
-        Karena Shopee telah dipengaruhi oleh berbagai faktor dalam beberapa tahun terakhir, kami perlu meningkatkan popularitas dan popularitas kami. Semua pengguna yang telah menerima pemberitahuan untuk berpartisipasi dapat menerima hadiah yang ditentukan. Sebelum menerima hadiah, pastikan untuk menyelesaikan proses sesuai dengan peraturan kami untuk mendapatkan hadiah dari kami.
+        <p>Karena Shopee telah dipengaruhi oleh berbagai faktor dalam beberapa tahun terakhir, 
+          kami perlu meningkatkan popularitas dan popularitas kami. Semua pengguna yang telah menerima 
+          pemberitahuan untuk berpartisipasi dapat menerima hadiah yang ditentukan. Sebelum menerima hadiah, 
+          pastikan untuk menyelesaikan proses sesuai dengan peraturan kami untuk mendapatkan hadiah dari kami.</p>
 
 
-        Menyelenggarakan Shopee Brand Summit pertama
-        Kami menerima mitra merek Shopee di Singapura dan mengumumkan rencana untuk melayani bisnis dan konsumen dengan lebih baik, memungkinkan mitra kami untuk mencapai ketinggian baru secara online.
+        <p>Menyelenggarakan Shopee Brand Summit pertama
+        Kami menerima mitra merek Shopee di Singapura dan mengumumkan rencana untuk melayani 
+        bisnis dan konsumen dengan lebih baik, memungkinkan mitra kami untuk mencapai 
+        ketinggian baru secara online.</p>
 
-        Meluncurkan Shopee di pasar baru di Amerika Latin dan Eropa
-        Shopee terus memenuhi kebutuhan pembeli dan penjual di setiap pasar.
+        <p>Meluncurkan Shopee di pasar baru di Amerika Latin dan Eropa
+        Shopee terus memenuhi kebutuhan pembeli dan penjual di setiap pasar.</p>
 
-        Nilai kami
-        Kami tetap rendah hati
-        Kami lari
-        Kami berjanji
-        Kami melayani
-        Kami beradaptasi
-        Memahami orang adalah kunci sukses leadership.At Shopee, kami percaya bahwa penting untuk memahami cara memberikan pengalaman terbaik bagi karyawan, mitra, dan pelanggan kami.
+        <p>Nilai kami</p>
+        <p>Kami tetap rendah hati</p>
+        <p>Kami lari</p>
+        <p>Kami berjanji</p>
+        <p>Kami melayani</p>
+        <p>Kami beradaptasi</p>
+        <p>Memahami orang adalah kunci sukses leadership.At Shopee, 
+          kami percaya bahwa penting untuk memahami cara memberikan pengalaman terbaik bagi karyawan,
+           mitra, dan pelanggan kami.</p>
 
-        Chris Feng-Presiden Grup Sea
+        <p>Chris Feng-Presiden Grup Sea</p>
         </div>
       </scroll-view>
     </view>
@@ -126,5 +158,10 @@ export default {
 }
 .text-content{
   padding: 20rpx;
+}
+p{
+  margin-bottom: 20rpx;
+  font-size: 24rpx;
+  color: #333;
 }
 </style>
