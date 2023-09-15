582c30784dbfbe3da9b6fa2d46dc3e6c6134cdfe
water <watershui122@gmail.com>
2023-09-15 17:52:31 +0700

    客服修改

--- info-ui/pages/onlineService.vue
+++ info-ui/pages/onlineService.vue
@@ -15,9 +15,15 @@
     >
     </u-navbar>
     <view class="wrap">
-      <view v-if="path">
+      <!-- <view v-if="path">
         <iframe :src="path" class="online"> </iframe>
+      </view> -->
+      <view class="kefu-box">
+        <image class="kefu-img" src="../static/img/kefu.jpg" />
       </view>
+      <view class="title">CHĂM SÓC KHÁCH HÀNG </view>
+      <view class="text">Thời gian làm việc : 8 giờ 30  đến 17 giờ </view>
+      <view class="btn" @click="changeBtn">Liên hệ</view>
     </view>
   </view>
 </template>
@@ -40,6 +46,9 @@ export default {
         }
       });
     },
+    changeBtn(){
+      this.$base.show("Hiện tại tất cả đường dây đều bận , bạn vui lòng chờ trong giây lát sẽ có nhân viên liên hệ hỗ trợ")
+    }
   },
 };
 </script>
@@ -49,5 +58,37 @@ export default {
   width: 100%;
   height: calc(100vh - 100rpx + var(--status-bar-height));
   border: none;
+}
+.kefu-box{
+  width: 100%;
+  .kefu-img{
+    width: 70%;
+    // height: 480rpx;
+    left: 15%;
+    top: 20%;
+  }
+}
+.title{
+  width: 100%;
+  text-align: center;
+  color: #bc2733;
+  font-size: 40rpx;
+  font-weight: 600;
+}
+.text{
+  width: 100%;
+  text-align: center;
+  color: #bc2733;
+  font-size: 32rpx;
+}
+.btn{
+  width: 80%;
+  height: 100rpx;
+  border-radius: 50rpx;
+  background-color: #bc2733;
+  color: #fff;
+  line-height: 100rpx;
+  text-align: center;
+  margin: 40rpx auto;
 }
 </style>
