c82def34160fd6603795c850e531e448b2089340
water <watershui122@gmail.com>
2023-09-20 17:47:16 +0700

    接口地址

--- info-ui/plugins/base.js
+++ info-ui/plugins/base.js
@@ -31,8 +31,8 @@ const PATH_URL =
   process.env.NODE_ENV === "development"
     ? "http://192.168.4.85:9522/" //本地测试
     // ? "https://as6666.top/chaoge-qtapi-prod/" //测试
-    : "https://as6666.top/chaoge-qtapi-prod/"; //app打包地址测试
-    // : "/yn-qtapi-prod/"; //越南101 线上h5打包地址
+    // : "https://as6666.top/chaoge-qtapi-prod/"; //app打包地址测试
+    : "/chaoge-qtapi-prod/"; //越南101 线上h5打包地址
 
 export const upload = (params) => {
   let header = {
